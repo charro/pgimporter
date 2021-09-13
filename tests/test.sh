7cdc6cadf1cf2e103c5672d77a2c5559a1fe4164
Miguel Rivero <charrinton@hotmail.com>
2021-09-13 22:11:26 +0200

    Update test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,6 @@
 # Create schema in both DBs (source and target)
 PGPASSWORD=postgres psql -h $1 -U postgres -f create.sql
-PGPASSWORD=postgres psql -h $2 -U postgres -f create.sql
+PGPASSWORD=postgres psql -h $2 -U postgres -p 5555 -f create.sql
 # Insert data
 for i in {1..1000}
 do
